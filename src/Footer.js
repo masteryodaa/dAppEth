3bd5d1b2d5107d43e61be654572f95c943702cc1
Ujjwal Kumar <kujjwal42@gmail.com>
2022-01-20 23:07:42 +0530

    naya update

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { BsGithub } from 'react-icons/bs';
+import { FaBitcoin } from 'react-icons/fa';
+import { FaEthereum } from 'react-icons/fa';
 
 function Footer() {
   return <div className='footer fixed-bottom'>
-    <a href="https://github.com/masteryodaa/dAppEth" target="_blank" rel='noreferrer'>
-      <div className="github text-center">Fork it at <BsGithub /></div>
+    <a className='link d-flex justify-content-center align-items-center my-2' href="https://github.com/masteryodaa/dAppEth" target="_blank" rel='noreferrer'>
+      <div className='mx-1'>Fork it at</div> <BsGithub />
     </a>
+
+    <div className="support my-1">
+      <div>Support me at </div>
+      <div className="bitcoin fw-bold"><FaBitcoin />15vnoxf6gH7WvotjakngexrspTzZVzofSB</div>
+      <div className="ether fw-bold"><FaEthereum />0x3ef627d16041c1cd2a853689269c3abe1d85153a</div>
+    </div>
+
+    
   </div>;
 }
 
